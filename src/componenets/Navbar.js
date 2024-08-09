775b84da9c0df555e3e170d0328d558a4d26645f
AmanUllah687 <Amanch687@gmail.com>
2024-08-09 16:54:08 +0500

    Styling React Router links

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
  export const  Navbar = () => {
+    const navlinkstyle = ({isActive}) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
  return (
     <nav>
-    <Link to='/'>Home</Link>
-    <Link to='/About'>About</Link>
+    <NavLink style={navlinkstyle} to='/'>Home</NavLink>
+    <NavLink style={navlinkstyle} to='/About'>About</NavLink>
     </nav>
  )
  }
