8796d911cb8a5b160a16f8d1961fc66a3208d6db
AmanUllah687 <Amanch687@gmail.com>
2024-08-09 12:47:54 +0500

    React Router Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./componenets/Home";
 import { About } from "./componenets/About";
+import { Navbar } from "./componenets/Navbar";
 function App() {
   return (
+    <>
+    <Navbar />
    <Routes>
     <Route path='/' element={<Home />} />
     <Route path='about' element={<About />} />
    </Routes>
+   </>
   );
 }
 
